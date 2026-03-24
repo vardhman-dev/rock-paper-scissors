31a85852baab4a4ef97bdec8bc0a5c20f7f4af92
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-03-24 16:47:39 +0000

    Get computer's choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3); // output : 0 or 1 or 2
+    
+    if(randomNumber === 0) {
+        return "Rock";
+    }
+    else if(randomNumber === 1) {
+        return "Papper";
+    }
+    else {
+        return "Scissors";
+    }
+}
