ea4be03bef2a820ff7f6f4345209c200aa17a378
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-05-25 14:48:33 +0000

    Finished game with DOM manipulation

--- script.js
+++ script.js
@@ -1,96 +1,186 @@
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*3); // output : 0 or 1 or 2
-    
-    if(randomNumber === 0) {
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3); // output : 0 or 1 or 2
+
+    if (randomNumber === 0) {
         return "Rock";
     }
-    else if(randomNumber === 1) {
-        return "Papper";
+    else if (randomNumber === 1) {
+        return "Paper";
     }
     else {
         return "Scissors";
     }
 }
 
-function getHumanChoice(){
-    let choice = +prompt("Enter a number : 0 for 'Rock', 1 for 'Papper', 2 for 'Scissors'");
-    
-    if(choice === 0){
-        return "Rock";
-    }
-    else if(choice === 1){
-        return "Papper";
-    }
-    else{
-        return "Scissors";
-    }
+function createButtons() {
+    const rockBtn = document.createElement("button");
+    const paperBtn = document.createElement("button");
+    const scissorBtn = document.createElement("button");
+
+    rockBtn.style.background = "gray";
+    rockBtn.style.color = "white";
+    rockBtn.textContent = "Rock";
+    rockBtn.style.height = "30px";
+    rockBtn.style.width = "60px";
+
+    paperBtn.style.background = "red";
+    paperBtn.style.color = "black";
+    paperBtn.textContent = "Paper";
+    paperBtn.style.height = "30px";
+    paperBtn.style.width = "60px";
+
+    scissorBtn.style.background = "gold";
+    scissorBtn.style.color = "black";
+    scissorBtn.textContent = "Scissor";
+    paperBtn.style.height = "30px";
+    paperBtn.style.width = "60px";
+
+    const div = document.createElement("div");
+    div.appendChild(rockBtn);
+    div.appendChild(paperBtn);
+    div.appendChild(scissorBtn);
+    div.style.display = "flex";
+    div.style.justifyContent = "space-around";
+    div.style.marginTop = "30px";
+
+    const body = document.querySelector("body");
+    body.appendChild(div);
+
+    return { rockBtn, paperBtn, scissorBtn };
 }
 
-let humanScore = 0;
+function listener() {
+
+    let buttonGroup = createButtons();
+
+    buttonGroup.rockBtn.addEventListener("click", () => {
+        playRound("Rock", getComputerChoice());
+    });
+
+    buttonGroup.paperBtn.addEventListener("click", () => {
+        playRound("Paper", getComputerChoice());
+    });
+
+    buttonGroup.scissorBtn.addEventListener("click", () => {
+        playRound("Scissors", getComputerChoice());
+    });
+}
+
+function createDisplay(){
+    const div1 = document.createElement("div");
+    div1.style.display = "flex"
+    div1.style.flexDirection = "column";
+    div1.style.justifyContent = "center";
+    div1.style.alignItems = "center";
+    div1.style.height = "auto";
+    div1.style.width = "auto";
+    div1.style.background = "red";
+    div1.style.margin = "auto";
+    div1.style.marginTop = "50px";
+    div1.classList.add("display");
+    const body1 = document.querySelector("body");
+    body1.appendChild(div1);
+    return div1;
+}
+
 let computerScore = 0;
+let humanScore = 0;
+listener();
+let display = createDisplay();
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice === "Rock"){
-        if(computerChoice === "Paper"){
+function playRound(humanChoice, computerChoice) {
+    
+    let result = null;
+
+    if (humanChoice === "Rock") {
+        if (computerChoice === "Paper") {
             computerScore += 1
-            console.log("Computer won!")
+            result = "compWin";
         }
-        else if(computerChoice === "Scissors"){
+        else if (computerChoice === "Scissors") {
             humanScore += 1
-            console.log("You won!!!")
+            result = "userWin";
         }
-        else{
-            console.log("Tie")
+        else {
+            result = "Tie";
         }
     }
-    else if(humanChoice === "Paper"){
-        if(computerChoice === "Rock"){
+    else if (humanChoice === "Paper") {
+        if (computerChoice === "Rock") {
             humanScore += 1
-            console.log("You won!!!")
+            result = "userWin";
         }
-        else if(computerChoice === "Scissors"){
+        else if (computerChoice === "Scissors") {
             computerScore += 1
-            console.log("Computer won!")
+            result = "compWin";
         }
-        else{
-            console.log("Tie")
+        else {
+            result = "Tie";
         }
     }
-    else{ // humanChoice === "Scissors"
-        if(computerChoice === "Paper"){
+    else { // humanChoice === "Scissors"
+        if (computerChoice === "Paper") {
             humanScore += 1
-            console.log("You won!!!")
+            result = "userWin";
         }
-        else if(computerChoice === "Rock"){
+        else if (computerChoice === "Rock") {
             computerScore += 1
-            console.log("Computer won!")
+            result = "compWin";
+        }
+        else {
+            result = "Tie";
+        }
+    }
+
+    let gameResult = document.querySelector(".display");
+    gameResult.innerHTML = "";
+    const p1 = document.createElement("p");
+
+    let gameFinish = false;
+
+    if(result === "userWin"){
+        if(humanScore >= 5){
+            gameFinish = true;
+            p1.textContent = "You won the game";
         }
         else{
-            console.log("Tie")
+        p1.textContent = `You won, score :  ${humanScore}`;
         }
+        gameResult.appendChild(p1);
+    } 
+    else if(result === "compWin"){
+        if(computerScore >= 5){
+            gameFinish = true;
+            p1.textContent = "Computer won the game";
+        }
+        else{
+        p1.textContent = `Computer won, score :  ${computerScore}`;
+        }
+        gameResult.appendChild(p1);
+    }  
+    else{ // result == "Tie"
+        p1.textContent = `Tie`;
+        gameResult.appendChild(p1);
+    }
+
+    if(gameFinish === true){
+        const resetBtn = document.createElement("button");
+        resetBtn.textContent = "Reset Game";
+        resetBtn.style.width = "100px";
+        resetBtn.style.height = "30px";
+        resetBtn.style.marginBottom = "16px"
+        resetBtn.addEventListener("click", () =>{
+            location.reload();
+            }
+        );
+        gameResult.appendChild(resetBtn);
+
+        let buttons = document.querySelectorAll("button")
+        buttons.forEach(btn => {
+            if(btn.textContent !== "Reset Game"){
+                btn.disabled = true;
+            }
+        });
     }
 }
 
-// Function to play 5 rounds of game and display score at the end with winner anouncement.
-function playGame(){
-    for(let i=1; i<=5;i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    console.log("Your Score : "+ humanScore);
-    console.log("Computer's Score : "+ computerScore);
-
-    if(computerScore < humanScore){
-        console.log("You won the game.")
-    }
-    else if(computerScore > humanScore){
-        console.log("Computer won the game.")
-    }
-    else{
-        console.log("The game was a tie.")
-    }
-
-    humanScore = 0;
-    computerScore = 0;
-    console.log("Score reset to 0, do you want to play another game ?");
-}
